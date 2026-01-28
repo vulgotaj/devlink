c15b54c14760306f3476a3c590d4dabcde06e203
Felipe Tajima <felipebtajima05@gmail.com>
2026-01-27 21:36:58 -0300

    Iniciando página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,23 @@
 export function Home() {
     return (
-        <div>
-            <h1>Página HOME</h1>
+        <div className="flex flex-col w-full py-4 items-center justify-center">
+            <h1 className="md:text-4xl text 3xl font-bold text-white mt-20">Felipe Tajima</h1>
+            <span className="text-gray-50 mb-5 mt-5 ">Veja meus links!</span>
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105">
+                    <a>
+                        <p className="text-base md:text-lg">
+                            Canal no Youtube
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="flex justify-center gap-3 my-4">
+
+                </footer>
+
+            </main>
         </div>
     )
 }
