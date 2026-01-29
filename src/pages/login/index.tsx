aa1f6b1c196a3d4854c49f8d13bf1653cdc30b64
Felipe Tajima <felipebtajima05@gmail.com>
2026-01-28 22:29:56 -0300

    Finalizando Login e criando proteção para admin

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,32 @@
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 import { Input } from '../../components/Input'
 import { type SubmitEvent, useState } from 'react'
 import { auth } from '../../services/firebaseConnection'
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     function handleSubmit(e: SubmitEvent) {
         e.preventDefault();
 
-        console.log({
-            email: email,
-            password: password
+        if(email === '' || password === '') {
+            alert("Preencha todos os campos!")
+            return;
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            console.log("LOGADO COM SUCESSO!")
+            navigate("/admin", { replace: true })
         })
+        .catch((error) => {
+            console.log("ERRO AO FAZER O LOGIN: ")
+            console.log(error);
+        })
+
     }
 
     return (
