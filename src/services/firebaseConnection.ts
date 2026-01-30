212519bff35ac6ecea670429266ac5b0286faac8
Felipe Tajima <felipebtajima05@gmail.com>
2026-01-30 19:38:18 -0300

    Preparando para deploy pt.2

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAP8DUd0GEMqDpC8qdYdLHHKzmnm3Jn_pY",
+  authDomain: "reactlinks-461f0.firebaseapp.com",
+  projectId: "reactlinks-461f0",
+  storageBucket: "reactlinks-461f0.firebasestorage.app",
+  messagingSenderId: "169478692056",
+  appId: "1:169478692056:web:5c34024469e517a971cbad"
 };
 
 const app = initializeApp(firebaseConfig);
